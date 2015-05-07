9bdf7e2b44a531155a81095da9244222dc3ab07a
petedigipulse <peter@digitalpulse.co.nz>
2015-05-08 11:01:26 +1200

    Set up customisable grid

--- js/maze.js
+++ js/maze.js
@@ -3,5 +3,32 @@
 function Maze(width, height) {
 	this.width = width;
 	this.height = height;
+
+	this.startX				= null;
+	this.startY    			= null;
+	this.startOrientation 	= null; 
+	this.endX				= null;
+	this.endY				= null; 
+
+	this.spaces = [];
+
+	var x, y;
+	for (x=1; x <= width; x += 1) {
+		this.spaces[x] = [];
+		for (y=1; y <= height; y += 1) {
+			this.spaces[x][y] = "(" + x + "," + y + ")";
+		}
+		
+	};
 };
 
+Maze.prototype.setStart = function(x, y, orientation) {
+	this.startX = x; 
+	this.startY = y; 
+	this.startOrientation = orientation;
+};
+
+Maze.prototype.setEnd = function(x, y) {
+	this.endY = y;
+	this.endX = x; 
+};
--- notes.md
+++ notes.md
@@ -8,3 +8,11 @@ mazespace.js
 
 define a function called Maze
 test it in the console
+set arguments in parameter
+assign co-ordinates and orientation to this
+create start and end function and call it from html.
+create an empty array called spaces 
+loop through y coords 
+loop through x coords
+create html element ( , ) and include  x & y 
+
